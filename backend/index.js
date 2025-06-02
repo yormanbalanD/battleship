55d9d8f3302fca194a6266fbb712a48e675ce68a
yormanmp3 <yormanmp3balan2@gmail.com>
2025-06-02 12:22:40 -0400

    Public arreglado

--- backend/index.js
+++ backend/index.js
@@ -383,7 +383,7 @@ class GameInstance {
 }
 
 
-app.use(express.static(path.join(__dirname, "..", "frontend", 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 io.on('connection', (socket) => {
     console.log(`Cliente conectado: ${socket.id}`);
